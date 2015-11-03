f4033214c0be633d055e36bdc1e58a1e29f9428b
pravin-teratech <pravin@teratech-solutions.com>
2015-11-03 19:06:03 +0530

    pravin add new class file DBConnection and DLBranch

---
+++ src/DataAccessLayer/DBConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccessLayer
+{
+    public class DBConnection
+    {
+        public SqlConnection getConnection()
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["conn"].ToString());
+            return conn;
+        }
+    }
+}
