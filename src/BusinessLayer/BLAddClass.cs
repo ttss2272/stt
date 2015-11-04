3a6ca3d864534f4aac67a8b5f381ae3011109ab0
pravin-teratech <pravin@teratech-solutions.com>
2015-11-04 12:49:30 +0530

    pravin create BL AddClass file

---
+++ src/BusinessLayer/BLAddClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccessLayer;
+using System.Data;
+using BusinessLayer;
+
+namespace BusinessLayer
+{
+    public class BLAddClass
+    {
+        DLAddClass obj_AddClass = new DLAddClass();
+        public string saveAddClass(int id, string ClassName, string ShortName, int Board, string Color)
+        {
+            string result = obj_AddClass.saveAddClass(id, ClassName, ShortName, Board, Color);
+            return result;
+        }
+    }
+}
