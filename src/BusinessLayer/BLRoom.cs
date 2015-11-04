5d833a4c55d5f59513b4aee47a15f446f5e66ece
ttss2272 <admin@teratech-solutions.com>
2015-11-04 11:46:56 +0530

    Pranjali BL & DL Room

---
+++ src/BusinessLayer/BLRoom.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccessLayer;
+using System.Data;
+
+namespace BusinessLayer
+{
+    public class BLRoom
+    {
+        DLRoom obj_Room = new DLRoom();
+
+
+        public string addRoom( string RoomName, string ShortName, int Capacity, string  Color, string IsActive, string IsDelete)
+        {
+            string result = obj_Room.addRoom( RoomName, ShortName , Capacity, Color, IsActive, IsDelete);
+            return result;
+        }
+    }
+}
---
+++ src/DataAccessLayer/DLRoom.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DataAccessLayer
+{
+    public class DLRoom
+    {
+       SqlConnection conn = new SqlConnection();
+        DBConnection con = new DBConnection();
+        public string addRoom(string RoomName, string ShortName, int Capacity, string Color, string IsActive, string IsDelete)
+        {
+            string result = null;
+            conn = con.getConnection();
+            SqlCommand cmd = new SqlCommand("Usp_AddRoom", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@RoomName", RoomName);
+            cmd.Parameters.AddWithValue("@ShortName", ShortName );
+            cmd.Parameters.AddWithValue("@Capacity", Capacity );
+            cmd.Parameters.AddWithValue("@IsActive", IsActive);
+            cmd.Parameters.AddWithValue("@IsDelete", IsDelete);
+            conn.Open();
+            result = cmd.ExecuteNonQuery().ToString();
+            conn.Close();
+            return result;
+
+    }
+}
