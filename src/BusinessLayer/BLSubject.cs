35906922c31303a7a5db91597a8eb6871ffff332
pritesh-teratech <pritesh@teratech-solutions.com>
2015-11-04 04:37:27 -0800

    PriTesh
    Save Subject

---
+++ src/BusinessLayer/BLSubject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using DataAccessLayer;
+
+namespace BusinessLayer
+{
+    public class BLSubject
+    {
+        DLSubject objSubject = new DLSubject();
+
+        public string SaveSubject(int SubjectID, string SubjectName, string SubjectShortName, int UpdatedByUserID, string UpdatedDate, int IsActive)
+        {
+            string Result = objSubject.SaveSubject(SubjectID, SubjectName, SubjectShortName, UpdatedByUserID, UpdatedDate, IsActive);
+                return Result;
+        }
+    }
+}
