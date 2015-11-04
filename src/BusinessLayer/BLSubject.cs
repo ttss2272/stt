732604383ba06322fc763a6dbb5842894c4c0ad2
pritesh-teratech <pritesh@teratech-solutions.com>
2015-11-04 06:52:06 -0800

    PriTesh
    Datagrid Bind

--- src/BusinessLayer/BLSubject.cs
+++ src/BusinessLayer/BLSubject.cs
@@ -16,5 +16,25 @@ namespace BusinessLayer
             string Result = objSubject.SaveSubject(SubjectID, SubjectName, SubjectShortName, UpdatedByUserID, UpdatedDate, IsActive);
                 return Result;
         }
+
+        //To Bind DropDown
+        public DataSet GetSubject(int SubjectID)
+        {
+            DataSet ds = objSubject.GetSubject(SubjectID);
+            return ds;
+        }
+
+         //To Bind Gridview
+        public DataSet BindSubject(int SubjectID)
+        {
+            DataSet ds = objSubject.BindSubject(SubjectID);
+            return ds;
+        }
+        //For Edit Details
+        public DataSet GetSubjectDetail(int SubjectID)
+        {
+            DataSet ds = objSubject.GetSubjectDetail(SubjectID);
+            return ds;
+        }
     }
 }
