a9f61d3a00acf5d26fa3f5cf10eec0397a2cbb76
pravin-teratech <pravin@teratech-solutions.com>
2015-11-04 18:54:07 +0530

    pravin create batch bl class file

---
+++ src/BusinessLayer/BLBatch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccessLayer;
+using System.Data;
+using BusinessLayer;
+
+namespace BusinessLayer
+{
+    
+    public class BLBatch
+    {
+        DLBatch obj_Batch = new DLBatch();
+        public string saveBatch(int id, int ClassID, string BatchName, string BatchCode)
+        {
+            string result = obj_Batch.saveBatch(id, ClassID, BatchName, BatchCode);
+               return result;
+        }
+    }
+}
