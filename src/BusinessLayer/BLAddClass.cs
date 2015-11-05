b8f26c63f996f18b8927d01ea2787d9c233609b2
pravin-teratech <pravin@teratech-solutions.com>
2015-11-05 11:41:52 +0530

    pravin create save button code and grid bind code

--- src/BusinessLayer/BLAddClass.cs
+++ src/BusinessLayer/BLAddClass.cs
@@ -11,10 +11,17 @@ namespace BusinessLayer
     public class BLAddClass
     {
         DLAddClass obj_AddClass = new DLAddClass();
-        public string saveAddClass(int id, string ClassName, string ShortName, int Board, string Color)
+        public string saveAddClass(int id, string ClassName, string ShortName, int Board, string Color, int UpdatedByUserID, string UpdatedDate, int IsActive)
         {
-            string result = obj_AddClass.saveAddClass(id, ClassName, ShortName, Board, Color);
+            string result = obj_AddClass.saveAddClass(id, ClassName, ShortName, Board, Color, UpdatedByUserID, UpdatedDate, IsActive);
             return result;
+        }
+
+        //To Bind Gridview
+        public DataSet BindClass(int ClassID)
+        {
+            DataSet ds = obj_AddClass.BindClass(ClassID);
+                return ds;
         }
     }
 }
--- src/BusinessLayer/BLBatch.cs
+++ src/BusinessLayer/BLBatch.cs
@@ -12,10 +12,16 @@ namespace BusinessLayer
     public class BLBatch
     {
         DLBatch obj_Batch = new DLBatch();
-        public string saveBatch(int id, int ClassID, string BatchName, string BatchCode)
+        public string saveBatch(int id, int ClassID, string BatchName, string BatchCode, int UpdatedByUserID, string UpdatedDate, int IsActive)
         {
-            string result = obj_Batch.saveBatch(id, ClassID, BatchName, BatchCode);
+            string result = obj_Batch.saveBatch(id, ClassID, BatchName, BatchCode, UpdatedByUserID, UpdatedDate, IsActive);
                return result;
+        }
+
+        public DataSet BindBatch(int BatchID)
+        {
+            DataSet ds = obj_Batch.BindBatch(BatchID);
+                return ds;
         }
     }
 }
