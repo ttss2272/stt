9267fcede82c7e9961fdcc0c712d5e5cbc693a6e
ttss2272 <admin@teratech-solutions.com>
2015-11-06 21:23:19 +0530

    Kalpesh Get, Delete code for user

--- src/SchoolManagement/User/User.xaml.cs
+++ src/SchoolManagement/User/User.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using BusinessLayer;
+using System.Data;
 
 namespace SchoolManagement.User
 {
@@ -28,8 +29,8 @@ namespace SchoolManagement.User
         }
 
         BLUser obj_User = new BLUser();
-        
-        int UserID, IsActive, IsDeleted, UserTypeID=1;
+
+        int UserID, IsActive, IsDeleted, UserTypeID = 1, UpID;
         string UserName, ContactNo, Address, MailId, LoginName, Password, UpdatedDate ;
 
         #region---------------------------Validate()-----------------------------------------
@@ -101,8 +102,16 @@ namespace SchoolManagement.User
         
         private void btnSave_Click(object sender, RoutedEventArgs e)
         {
-            SetParameters();
-            SaveUpdateUser();
+            try
+            {
+                SetParameters();
+                SaveUpdateUser();
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Exception Error");
+            }
         }
 
         private void SaveUpdateUser()
@@ -122,7 +131,7 @@ namespace SchoolManagement.User
 
         private void SetParameters()
         {
-            UserID = 0;
+            UserID = UpID;
             UserName = txtFullName.Text.Trim();
             ContactNo = txtContactNo.Text.Trim();
             Address = txtAddress.Text.Trim();
@@ -145,22 +154,130 @@ namespace SchoolManagement.User
 
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                MessageBoxResult Result = MessageBox.Show("Do You Really Want To Delete?", "Delete User", MessageBoxButton.YesNo, MessageBoxImage.Information);
+                if (Result.Equals(MessageBoxResult.Yes))
+                {
+                    SetParameters();
+                    DeleteUser();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Exception Error");
+            }
+        }
 
+        private void DeleteUser()
+        {
+            if (UpID != 0)
+            {
+                UserID = UpID;
+                UpdatedDate = DateTime.Now.ToString();
+                string Result = obj_User.DeleteUser(UserID, UpdatedDate);
+                if (Result == "Deleted Sucessfully.")
+                {
+                    MessageBox.Show(Result, "Delete Sucessfully", MessageBoxButton.OK, MessageBoxImage.Information);
+                    ClearFields();
+                }
+                else
+                {
+                    MessageBox.Show(Result, "Error To Delete", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please Select Subject From Subject", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+            }
         }
 
         private void btnClear_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Exception Error");
+            }
+        }
 
+        private void ClearFields()
+        {
+            txtFullName.Text = "";
+            txtAddress.Text = "";
+            txtContactNo.Text = "";
+            txtEmailID.Text = "";
+            txtLoginID.Text = "";
+            txtPassword.Text = "";
+            cmbActive.IsChecked = true;
+            bindUserGrid();
+            UpID = 0;
+            btnDelete.IsEnabled = false;
+            txtSearchUser.Text = "";
+            btnSave.Content = "Save";
         }
 
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                bindUserGrid();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Exception Error");
+            }
         }
 
-        private void gvUser_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void bindUserGrid()
         {
+            DataSet ds = obj_User.GetUser(0,txtSearchUser.Text);
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                gvUser.ItemsSource = ds.Tables[0].DefaultView;
+            }
+            else
+            {
+                gvUser.ItemsSource = null;
+                MessageBox.Show("Data Not Found", "Message");
+                txtSearchUser.Text = "";
+            }
+        }
 
+        private void Row_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                object item = gvUser.SelectedItem;
+                UpID = Convert.ToInt32(((System.Data.DataRowView)(gvUser.CurrentItem)).Row.ItemArray[0].ToString());
+                txtFullName.Text = ((System.Data.DataRowView)(gvUser.CurrentItem)).Row.ItemArray[1].ToString();
+                txtAddress.Text = ((System.Data.DataRowView)(gvUser.CurrentItem)).Row.ItemArray[2].ToString();
+                txtContactNo.Text = ((System.Data.DataRowView)(gvUser.CurrentItem)).Row.ItemArray[3].ToString();
+                txtEmailID.Text = ((System.Data.DataRowView)(gvUser.CurrentItem)).Row.ItemArray[4].ToString();
+                txtLoginID.Text = ((System.Data.DataRowView)(gvUser.CurrentItem)).Row.ItemArray[5].ToString();
+                txtPassword.Text = ((System.Data.DataRowView)(gvUser.CurrentItem)).Row.ItemArray[6].ToString();
+                bool act = Convert.ToBoolean(((System.Data.DataRowView)(gvUser.CurrentItem)).Row.ItemArray[10].ToString());
+                bool del = Convert.ToBoolean(((System.Data.DataRowView)(gvUser.CurrentItem)).Row.ItemArray[11].ToString());
+
+                if (act == true && del == false)
+                {
+                    cmbActive.IsChecked = true;
+                }
+                else if (act == false && del == true)
+                {
+                    cmbActive.IsChecked = false;
+                }
+                btnDelete.IsEnabled = true;
+                btnSave.Content = "Update";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(),"Exception Error");
+            }
         }
     }
 }
