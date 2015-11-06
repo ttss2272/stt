85ed13a371a56bfb6d02ecd41f13350cc671ec89
ttss2272 <admin@teratech-solutions.com>
2015-11-06 17:53:16 +0530

    Kalpesh Save, Get, Delete code for user

---
+++ src/BusinessLayer/BLUser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using DataAccessLayer;
+
+namespace BusinessLayer
+{
+    public class BLUser
+    {
+        DLUser obj_User = new DLUser();
+
+        public string SaveUser(int UserID, string UserName, string ContactNo, string Address, string MailId, string LoginName, string Password, string UpdatedDate, int UserTypeID, int IsActive, int IsDeleted)
+        {
+            string result = obj_User.SaveUser(UserID,  UserName,  ContactNo,  Address,  MailId,  LoginName,  Password,   UpdatedDate,  UserTypeID,  IsActive,  IsDeleted);
+            return result;
+        }
+
+        public DataSet GetUser(int UserID, string UserName)
+        {
+            DataSet getUser = obj_User.GetUser(UserID, UserName);
+            return getUser;
+        }
+
+        public string DeleteUser(int UserID, string UpdatedDate)
+        {
+            string result = obj_User.DeleteUser(UserID, UpdatedDate);
+            return result;
+        }
+    }
+}
