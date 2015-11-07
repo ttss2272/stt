b4fb377bdc50286241e5221ed29b879bae1bd7f1
pravin-teratech <pravin@teratech-solutions.com>
2015-11-07 20:27:05 +0530

    pravin changes in update code

--- src/BusinessLayer/BLBatch.cs
+++ src/BusinessLayer/BLBatch.cs
@@ -12,7 +12,7 @@ namespace BusinessLayer
     public class BLBatch
     {
         DLBatch obj_Batch = new DLBatch();
-        public string saveBatch(int BatchID, int ClassID, string BatchName, string BatchCode, int LectureDuration, int IsLunchBreak, int LunchBreakStartTime, int LunchBreakEndTime, int MaxNoLecturesDay, int MaxNoLecturesWeek, int IsAllowMoreThanOneLectInBatch, int MaxNoOfLecureInRow, int UpdatedByUserID, string UpdatedDate, int IsActive, int IsDeleted)
+        public string saveBatch(int BatchID, int ClassID, string BatchName, string BatchCode, int LectureDuration, int IsLunchBreak, string LunchBreakStartTime, string LunchBreakEndTime, int MaxNoLecturesDay, int MaxNoLecturesWeek, int IsAllowMoreThanOneLectInBatch, int MaxNoOfLecureInRow, int UpdatedByUserID, string UpdatedDate, int IsActive, int IsDeleted)
         {
             string result = obj_Batch.saveBatch(BatchID, ClassID, BatchName, BatchCode, LectureDuration, IsLunchBreak, LunchBreakStartTime, LunchBreakEndTime, MaxNoLecturesDay, MaxNoLecturesWeek, IsAllowMoreThanOneLectInBatch, MaxNoOfLecureInRow, UpdatedByUserID, UpdatedDate, IsActive, IsDeleted);
                return result;
@@ -28,9 +28,10 @@ namespace BusinessLayer
         public string DeleteBatch(int BatchID, int UpdatedByUserID, string UpdatedDate)
         {
             string result = obj_Batch.DeleteBatch(BatchID, UpdatedByUserID, UpdatedDate);
-               return result;
+            return result;
         }
 
+        //For Edit Details
         public DataSet GetBatchDetail(string BatchName, string BatchCode)
         {
             DataSet ds = obj_Batch.GetBatchDetail(BatchName, BatchCode);
