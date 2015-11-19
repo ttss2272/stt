3226cadb2495524acee4c3f6d5ad2a850bb340fe
pranjali-teratech <pranjali@teratech-solutions.com>
2015-11-19 19:51:33 +0530

    pranjali coding

--- src/DataAccessLayer/DLRoom.cs
+++ src/DataAccessLayer/DLRoom.cs
@@ -44,36 +44,30 @@ namespace DataAccessLayer
             return result;
 
         }
-        public string UpdateRoom(int RoomId, string RoomName, string ShortName, string Color1, int Capacity, int BranchID, int UpdatedByUserID, string UpdatedDate, int IsActive, int IsDeleted, int MaxLectday, int MaxLectWeek, int MaxLectRow, DateTime STime, DateTime ETime, int IsAllow)
+        
+        public string SaveRoomAvailibility(int RoomID, string Day, string StartTime, string EndTime, int UpdatedByUserID, string UpdatedDate, int IsActive, int IsDeleted)
         {
-            string result = null;
+            string Result = null;
             conn = con.getConnection();
-            SqlCommand cmd = new SqlCommand("SaveRoom_SP", conn);
+            SqlCommand cmd = new SqlCommand("SaveRoomAvailable_SP", conn);
             cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.AddWithValue("@RoomID", RoomId);
-            cmd.Parameters.AddWithValue("@RoomName", RoomName);
-            cmd.Parameters.AddWithValue("@RoomShortName", ShortName);
-            cmd.Parameters.AddWithValue("@Color", Color1);
-            cmd.Parameters.AddWithValue("@Capacity", Capacity);
-            cmd.Parameters.AddWithValue("@BranchID", BranchID);
-            //  cmd.Parameters.AddWithValue("@CreatedByUserID",CreatedByUserId);
+
+            cmd.Parameters.AddWithValue("@RoomID", RoomID);
+            cmd.Parameters.AddWithValue("@Day", Day);
+            cmd.Parameters.AddWithValue("@StartTime", StartTime);
+            cmd.Parameters.AddWithValue("@EndTime", EndTime);
             cmd.Parameters.AddWithValue("@UpdatedByUserID", UpdatedByUserID);
-            //    cmd.Parameters.AddWithValue("@CreatedDate", CreatedDate);
             cmd.Parameters.AddWithValue("@UpdatedDate", UpdatedDate);
             cmd.Parameters.AddWithValue("@IsActive", IsActive);
             cmd.Parameters.AddWithValue("@IsDeleted", IsDeleted);
-            cmd.Parameters.AddWithValue("@MaxLectDay", MaxLectday);
-            cmd.Parameters.AddWithValue("@MaxLectWeek", MaxLectWeek);
-            cmd.Parameters.AddWithValue("@MaxLectRow", MaxLectRow);
-            cmd.Parameters.AddWithValue("@STime", STime);
-            cmd.Parameters.AddWithValue("@ETime", ETime);
-            cmd.Parameters.AddWithValue("@IsAllow", IsAllow);
+
             conn.Open();
-            result = cmd.ExecuteScalar().ToString();
+            Result = cmd.ExecuteScalar().ToString();
             conn.Close();
-            return result;
+            return Result;
 
         }
+
         // To Bind Grid 
         public DataSet BindFullGrid(int RoomID,string BranchName, string RoomName)
         {
@@ -112,6 +106,41 @@ namespace DataAccessLayer
 
         }
 
+        public DataSet BindRoomAvail()
+        {
+            conn = con.getConnection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("GetNoOfDaysRoomAvailable_SP", conn);
+
+
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter sqlDa = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sqlDa.Fill(ds);
+            conn.Close();
+            return ds;
+        }
+
+        public DataSet GetRoomAvailableDetail(int RoomID)
+        {
+            conn = con.getConnection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("GetRoomAvailableDetail_SP", conn);
+
+
+            cmd.Parameters.AddWithValue("@RoomID", RoomID);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter sqlDa = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sqlDa.Fill(ds);
+            conn.Close();
+            return ds;
+
+        }
 
         public DataSet BindRoomName()
         {
@@ -124,6 +153,24 @@ namespace DataAccessLayer
             SqlDataAdapter sqlDa = new SqlDataAdapter(cmd);
             DataSet ds = new DataSet();
 
+            sqlDa.Fill(ds);
+            conn.Close();
+            return ds;
+        }
+
+        public DataSet BindRoomDropDown(int RoomID)
+        {
+            conn = con.getConnection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("BindRoomName_SP", conn);
+
+
+            cmd.Parameters.AddWithValue("@RoomID", RoomID);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter sqlDa = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
             sqlDa.Fill(ds);
             conn.Close();
             return ds;
