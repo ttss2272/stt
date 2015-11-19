d383ed81446a8e6a02ac8bdd9665051b5cd349b3
pritesh-teratech <pritesh@teratech-solutions.com>
2015-11-18 22:27:08 -0800

    Pritesh
    design

---
+++ src/SchoolManagement/Batch/BatchAvailibility.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SchoolManagement.Batch
+{
+    /// <summary>
+    /// Interaction logic for BatchAvailibility.xaml
+    /// </summary>
+    public partial class BatchAvailibility : Window
+    {
+        public BatchAvailibility()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Copy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void chkAvailAllDay_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void chkAvailSameTime_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void chkMon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void chkTue_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void chkWed_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void chkThru_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void chkFri_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void chkSat_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void chkSun_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void chkStartHrs_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void chkStartMin_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void chkEndhrs_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void EndMin_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
