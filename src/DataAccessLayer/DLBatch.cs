48ed70341759585bf92f8894c62222dffc09ff73
pravin-teratech <pravin@teratech-solutions.com>
2015-11-20 19:19:59 +0530

    PRAVIN CREATE BIND FUNCTION CODE

--- src/DataAccessLayer/DLBatch.cs
+++ src/DataAccessLayer/DLBatch.cs
@@ -205,5 +205,22 @@ namespace DataAccessLayer
         {
             throw new NotImplementedException();
         }
+
+        public DataSet BindBatchAvail()
+        {
+            conn = con.getConnection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("GetNoOfDaysBatchAvailable_SP", conn);
+
+
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter sqlDa = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sqlDa.Fill(ds);
+            conn.Close();
+            return ds;
+        }
     }
 }
