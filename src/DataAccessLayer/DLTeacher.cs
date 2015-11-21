dd1081f6e7768ff794480d910c2ac5ab79c7fc89
pritesh-teratech <pritesh@teratech-solutions.com>
2015-11-21 07:06:20 -0800

    PriTesh

--- src/DataAccessLayer/DLTeacher.cs
+++ src/DataAccessLayer/DLTeacher.cs
@@ -188,5 +188,7 @@ namespace DataAccessLayer
             conn.Close();
             return ds;
         }
+
+       
     }
 }
---
+++ src/DataAccessLayer/DLTeacherSubject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccessLayer
+{
+    public class DLTeacherSubject
+    {
+        DBConnection con = new DBConnection();
+        SqlConnection conn = new SqlConnection();
+
+        public DataSet GetBatchBySubject(int BranchID, int SubjectID)
+        {
+            conn = con.getConnection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("GetBatchOnSubject_SP", conn);
+
+
+            cmd.Parameters.AddWithValue("@BranchID", BranchID);
+            cmd.Parameters.AddWithValue("@SubjectID", SubjectID);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter sqlDa = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sqlDa.Fill(ds);
+            conn.Close();
+            return ds;
+        }
+    }
+}
