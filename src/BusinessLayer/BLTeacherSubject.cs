31d243155c0756b97b775761d3fef65ad237a022
pritesh-teratech <pritesh@teratech-solutions.com>
2015-11-21 07:06:51 -0800

    PriTesh

---
+++ src/BusinessLayer/BLTeacherSubject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using DataAccessLayer;
+
+namespace BusinessLayer
+{
+    public class BLTeacherSubject
+    {
+        DLTeacherSubject objTeacherSubject = new DLTeacherSubject();
+        public DataSet GetBatchBySubject(int BranchID, int SubjectID)
+        {
+            DataSet ds = objTeacherSubject.GetBatchBySubject(BranchID, SubjectID);
+            return ds;
+        }
+    }
+}
