c78a8ec01d4e4971295bbbb62dddf738708732a4
pravin-teratech <pravin@teratech-solutions.com>
2015-11-28 12:41:42 +0530

    PRAVIN remove all bug to bind drop down

--- src/BusinessLayer/BLTimeTable.cs
+++ src/BusinessLayer/BLTimeTable.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
+using DataAccessLayer;
 
 namespace BusinessLayer
 {
     public class BLTimeTable
     {
+        DLTimeTable objTimeTable = new DLTimeTable();
+        public string SaveTimeTable(int TimeTableID, int BatchID, int RoomID, int TeacherSubjectID, string Day, string LectStartTime, string LectEndTime,int UpdatedByUserID, string UpdatedDate, int IsActive, int IsDeleted)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataSet BindTimeTable()
+        {
+            DataSet ds = objTimeTable.BindTimeTable();
+            return ds;
+        }
     }
 }
