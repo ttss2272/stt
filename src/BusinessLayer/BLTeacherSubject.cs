e1273ca4e65fc6eddc88c8530f928d99da7b5e1c
pritesh-teratech <pritesh@teratech-solutions.com>
2015-11-30 20:15:50 -0800

    PriTesh

--- src/BusinessLayer/BLTeacherSubject.cs
+++ src/BusinessLayer/BLTeacherSubject.cs
@@ -10,10 +10,16 @@ namespace BusinessLayer
     public class BLTeacherSubject
     {
         DLTeacherSubject objTeacherSubject = new DLTeacherSubject();
-        public DataSet GetBatchBySubject(int BranchID, int SubjectID)
+        public DataSet GetBatchBySubject(int BranchID, int SubjectID,int TeacherID)
         {
-            DataSet ds = objTeacherSubject.GetBatchBySubject(BranchID, SubjectID);
+            DataSet ds = objTeacherSubject.GetBatchBySubject(BranchID, SubjectID,TeacherID);
             return ds;
+        }
+
+        public string SaveTeacherSubject(int TeacherID, int BatchsubjectID, int UpdatedByUserID, string UpdatedDate, int IsActive, int IsDeleted)
+        {
+            string Result = objTeacherSubject.SaveTeacherSubject(TeacherID, BatchsubjectID, UpdatedByUserID, UpdatedDate, IsActive, IsDeleted);
+            return Result;
         }
     }
 }
