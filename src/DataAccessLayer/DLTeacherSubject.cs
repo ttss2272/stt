687dbbc481655a6810af360bd9aab6e5f92dbea2
pritesh-teratech <pritesh@teratech-solutions.com>
2015-11-30 20:16:14 -0800

    PriTesh

--- src/DataAccessLayer/DLTeacherSubject.cs
+++ src/DataAccessLayer/DLTeacherSubject.cs
@@ -12,7 +12,7 @@ namespace DataAccessLayer
         DBConnection con = new DBConnection();
         SqlConnection conn = new SqlConnection();
 
-        public DataSet GetBatchBySubject(int BranchID, int SubjectID)
+        public DataSet GetBatchBySubject(int BranchID, int SubjectID,int TeacherID)
         {
             conn = con.getConnection();
             conn.Open();
@@ -22,6 +22,7 @@ namespace DataAccessLayer
 
             cmd.Parameters.AddWithValue("@BranchID", BranchID);
             cmd.Parameters.AddWithValue("@SubjectID", SubjectID);
+            cmd.Parameters.AddWithValue("@TeacherID", TeacherID);
             cmd.CommandType = CommandType.StoredProcedure;
 
             SqlDataAdapter sqlDa = new SqlDataAdapter(cmd);
@@ -29,6 +30,28 @@ namespace DataAccessLayer
             sqlDa.Fill(ds);
             conn.Close();
             return ds;
+        }
+
+        public string SaveTeacherSubject(int TeacherID, int BatchsubjectID, int UpdatedByUserID, string UpdatedDate, int IsActive, int IsDeleted)
+        {
+            string Result = null;
+            conn = con.getConnection();
+            SqlCommand cmd = new SqlCommand("SaveTeacherBatchSubject_SP", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@TeacherID", TeacherID);
+
+            cmd.Parameters.AddWithValue("@BatchsubjectID", BatchsubjectID);
+
+            cmd.Parameters.AddWithValue("@UpdatedByUserID", UpdatedByUserID);
+            cmd.Parameters.AddWithValue("@UpdatedDate", UpdatedDate);
+            cmd.Parameters.AddWithValue("@IsActive", IsActive);
+            cmd.Parameters.AddWithValue("@IsDeleted", IsDeleted);
+
+            conn.Open();
+            Result = cmd.ExecuteScalar().ToString();
+            conn.Close();
+            return Result;
         }
     }
 }
