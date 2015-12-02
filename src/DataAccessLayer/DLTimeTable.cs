285f3f60609a5559b25a5857f36e96839095701d
pravin-teratech <pravin@teratech-solutions.com>
2015-12-02 17:14:38 +0530

    pravin change in day binding function

--- src/DataAccessLayer/DLTimeTable.cs
+++ src/DataAccessLayer/DLTimeTable.cs
@@ -27,7 +27,7 @@ namespace DataAccessLayer
             return ds;
         }
 
-        public DataSet BindDay(int BatchID,int RoomID,int TeacherID)
+        public DataSet BindDay(int BatchID, int RoomID, int TeacherID)
         {
             conn = con.getConnection();
             conn.Open();
@@ -42,45 +42,6 @@ namespace DataAccessLayer
             sqlDa.Fill(ds);
             conn.Close();
             return ds;
-        }
-
-        public DataSet BindTimeSlot(int BatchAvailableID,int BatchID, int Day)
-        {
-            conn = con.getConnection();
-            conn.Open();
-
-            SqlCommand cmd = new SqlCommand("BindTimeSlotDropDown_SP", conn);
-            cmd.Parameters.AddWithValue("@BatchAvailableID", BatchAvailableID);
-            cmd.Parameters.AddWithValue("@BatchID", BatchID);
-            cmd.Parameters.AddWithValue("@Day", Day); 
-            cmd.CommandType = CommandType.StoredProcedure;
-            SqlDataAdapter sqlDa = new SqlDataAdapter(cmd);
-            DataSet ds = new DataSet();
-            sqlDa.Fill(ds);
-            conn.Close();
-            return ds;
-        }
-
-        public string SaveTimeTable(int TimeTableID, DateTime TTStartTime, int BatchID, int UpdatedByUserID, string UpdatedDate, int IsActive, int IsDeleted)
-        {
-            string Result = null;
-
-            conn = con.getConnection();
-            SqlCommand cmd = new SqlCommand("SaveTimeTable_SP", conn);
-            cmd.CommandType = CommandType.StoredProcedure;
-
-            cmd.Parameters.AddWithValue("@TimeTableID",TimeTableID);
-            cmd.Parameters.AddWithValue("@TimeTableStartDate", TTStartTime);
-            cmd.Parameters.AddWithValue("@BatchID",BatchID);
-            cmd.Parameters.AddWithValue("@UpdatedByUserID", UpdatedByUserID);
-            cmd.Parameters.AddWithValue("@UpdatedDate", UpdatedDate);
-            cmd.Parameters.AddWithValue("@IsActive",IsActive);
-            cmd.Parameters.AddWithValue("@IsDeleted", IsDeleted);
-
-            conn.Open();
-            Result = cmd.ExecuteScalar().ToString();
-            conn.Close();
-            return Result;
         }
     }
 }
