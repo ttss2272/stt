af6f477ee58ec92a002f56b84a1f9537b99156cf
pravin-teratech <pravin@teratech-solutions.com>
2015-12-02 16:02:46 +0530

    pravin change in day binding function

--- src/DataAccessLayer/DLTimeTable.cs
+++ src/DataAccessLayer/DLTimeTable.cs
@@ -27,15 +27,15 @@ namespace DataAccessLayer
             return ds;
         }
 
-        public DataSet BindDay(int BatchID)
+        public DataSet BindDay(int BatchID,int RoomID,int TeacherID)
         {
             conn = con.getConnection();
             conn.Open();
 
             SqlCommand cmd = new SqlCommand("BindDay_SP", conn);
             cmd.Parameters.AddWithValue("@BatchID", BatchID);
-            //cmd.Parameters.AddWithValue("@RoomID", RoomID);
-            //cmd.Parameters.AddWithValue("@TeacherID", TeacherID);
+            cmd.Parameters.AddWithValue("@RoomID", RoomID);
+            cmd.Parameters.AddWithValue("@TeacherID", TeacherID);
             cmd.CommandType = CommandType.StoredProcedure;
             SqlDataAdapter sqlDa = new SqlDataAdapter(cmd);
             DataSet ds = new DataSet();
