e71e77d944e84127acffbf480382bf37356b5ac8
sameer-teratech1 <sameer@teratech-solutions.com>
2015-12-03 19:23:53 +0530

    Sameer Add Login window

---
+++ src/BusinessLayer/BLLogin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using DataAccessLayer;
+
+namespace BusinessLayer
+{
+    public class BLLogin
+    {
+        DLLogin obj_Login = new DLLogin();
+        public DataSet GetLoginDetails()
+        {
+            DataSet ds = obj_Login.GetLoginDetails();
+            return ds;
+        }
+    }
+}
