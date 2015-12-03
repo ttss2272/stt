753e5c09e038769f3e656417b50e99108a464ff3
sameer-teratech1 <sameer@teratech-solutions.com>
2015-12-03 19:24:34 +0530

    Sameer Add Login window

---
+++ src/DataAccessLayer/DLLogin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccessLayer
+{
+  public class DLLogin
+    {
+        SqlConnection con = new SqlConnection();
+        DBConnection conn = new DBConnection();
+
+        public DataSet GetLoginDetails()
+        {
+            con = conn.getConnection();
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("GetLoginDetails_SP", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter sqlDa = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sqlDa.Fill(ds);
+            con.Close();
+            return ds;
+        }
+    }
+}
