089371f03cee321f8f0f907796975bd9ff2e8a5a
pravin-teratech <pravin@teratech-solutions.com>
2015-12-04 18:40:23 +0530

    pravin & pranjali create code of Save Button

--- src/BusinessLayer/BLTimeTable.cs
+++ src/BusinessLayer/BLTimeTable.cs
@@ -11,9 +11,9 @@ namespace BusinessLayer
     {
         DLTimeTable objTimeTable = new DLTimeTable();
 
-        public string SaveTimeTable(int TimeTableID,DateTime TTStartTime, int BatchID,int UpdatedByUserID, string UpdatedDate, int IsActive, int IsDeleted)
+        public string SaveTimeTable(int TimeTableID,int TimeTableDetailID,DateTime TTStartDate, int BatchID,int RoomID,string Day,string LectStartTime,string LectEndTime,int TeacherSubjectID,int UpdatedByUserID, string UpdatedDate, int IsActive, int IsDeleted)
         {
-            string Result = objTimeTable.SaveTimeTable(TimeTableID,TTStartTime,BatchID,UpdatedByUserID,UpdatedDate,IsActive,IsDeleted);
+            string Result = objTimeTable.SaveTimeTable(TimeTableID, TimeTableDetailID, TTStartDate, BatchID, RoomID, Day, LectStartTime, LectEndTime, TeacherSubjectID, UpdatedByUserID, UpdatedDate, IsActive, IsDeleted);
             return Result;
         }
 
