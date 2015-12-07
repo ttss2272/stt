650fe5591b5012d93265679eddf6a6326b643999
pritesh-teratech <pritesh@teratech-solutions.com>
2015-12-06 23:01:15 -0800

    Pritesh
    Login validation and messages

--- src/DataAccessLayer/DLLogin.cs
+++ src/DataAccessLayer/DLLogin.cs
@@ -8,22 +8,39 @@ using System.Data.SqlClient;
 namespace DataAccessLayer
 {
   public class DLLogin
-    {
-        SqlConnection con = new SqlConnection();
+  {
+      /*
+         * Created By :- Pritesh D. Sortee
+         * Created Date :-07 Dec 2015
+         *  Purpose:- Declare Objects
+         */
+      #region----------------------------------Declare Objects-----------------------------------------
+      SqlConnection con = new SqlConnection();
         DBConnection conn = new DBConnection();
+      #endregion
 
-        public DataSet GetLoginDetails()
+        /*
+         * Updated by :- Pritesh D. Sortee
+         * Updated Date :-07 Dec 2015
+         * Update Purpose:- send parameters
+         */
+        #region-------------------------------------GetLoginDetails----------------------------------------------
+        public DataSet GetLoginDetails(string UserName,string Pass,string CurrentDate)
         {
             con = conn.getConnection();
             con.Open();
 
             SqlCommand cmd = new SqlCommand("GetLoginDetails_SP", con);
             cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@UserName", UserName);
+            cmd.Parameters.AddWithValue("@Pass", Pass);
+            cmd.Parameters.AddWithValue("@CurrentDate", CurrentDate);
             SqlDataAdapter sqlDa = new SqlDataAdapter(cmd);
             DataSet ds = new DataSet();
             sqlDa.Fill(ds);
             con.Close();
             return ds;
         }
+        #endregion
     }
 }
