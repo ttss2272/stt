1a5a579b15d183133cf7aa6ad5a976bfc25c5cf0
pritesh-teratech <pritesh@teratech-solutions.com>
2015-12-06 23:01:58 -0800

    PriTesh
    Login validation and work properly

--- src/BusinessLayer/BLLogin.cs
+++ src/BusinessLayer/BLLogin.cs
@@ -9,11 +9,26 @@ namespace BusinessLayer
 {
     public class BLLogin
     {
+        /*
+         * Created By:-
+         * Created Date:- 
+         * Purpose:- Declare Objects
+         */
+        #region-----------------------------Objects-----------------------
         DLLogin obj_Login = new DLLogin();
-        public DataSet GetLoginDetails()
+        #endregion
+
+        /*
+         * Updated by :- Pritesh D. Sortee
+         * Updated Date :-07 Dec 2015
+         * Update Purpose:- send parameters to Stored procedure
+         */
+        #region------------------------------------------GetloginDetails-----------------------------------------
+        public DataSet GetLoginDetails(string UserName, string Pass,string CurrentDate)
         {
-            DataSet ds = obj_Login.GetLoginDetails();
+            DataSet ds = obj_Login.GetLoginDetails(UserName,Pass,CurrentDate);
             return ds;
         }
+        #endregion
     }
 }
