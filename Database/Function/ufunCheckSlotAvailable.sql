08e5c62af0d902798e59e69793e81871677c74a9
pritesh-teratech <pritesh@teratech-solutions.com>
2015-12-08 01:31:00 -0800

    PriTesh

--- Database/Function/ufunCheckSlotAvailable.sql
+++ Database/Function/ufunCheckSlotAvailable.sql
@@ -10,16 +10,18 @@ AS
 
 BEGIN
 	DECLARE @ret int;
-	
+	--Duplicate Time Entry for same day Same Slot time Start time and End Time
 	IF EXISTS (SELECT BranchLectureDetailID FROM BranchLectureDetail where BranchID=@BranchID AND [DayName]=@Day AND LectureStartTime=@SlotStartTime AND LectureEndTime=@SlotEndTime)
 		BEGIN 
 			SET @ret =1
 		END
-	ELSE IF EXISTS (SELECT BranchLectureDetailID FROM BranchLectureDetail where BranchID=@BranchID AND [DayName]=@Day AND LectureStartTime=@SlotStartTime AND LectureEndTime=@SlotEndTime)
+	--Check Whether entered time is in between existing time slot
+	ELSE IF EXISTS (SELECT BranchLectureDetailID FROM BranchLectureDetail where [DayName] =@Day AND BranchID=@BranchID AND (CAST (@SlotStartTime AS TIME)  BETWEEN CAST (LectureStartTime AS TIME) and CAST (LectureEndTime AS TIME)OR CAST (@SlotEndTime AS TIME) BETWEEN CAST (LectureStartTime AS TIME) and CAST (LectureEndTime AS TIME)))
+	
 		BEGIN
 			SET @ret=2
 		END 
-	
+	--no duplication or in between of time
 	ELSE
 		BEGIN
 			SET @ret=0
